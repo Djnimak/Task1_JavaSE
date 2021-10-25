69372c5a5d86c12148280fb4b10e7322554c068b
Djnimak <79873448+Djnimak@users.noreply.github.com>
2021-10-25 21:31:49 +0300

    Completed Lecture27

---
+++ Collections/Lecture27/src/com/nikita/homework/Animal.java
@@ -0,0 +1,50 @@
+package com.nikita.homework;
+
+import java.util.Objects;
+
+public class Animal {
+    String name;
+    int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return getAge() == animal.getAge() && getName().equals(animal.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
